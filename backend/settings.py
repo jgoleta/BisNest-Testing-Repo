03b108a4f578b8b065a3b8771686c9245220c27f
Mcleilan Jay Cosa <mjcosa@gbox.adnu.edu.ph>
2025-10-22 12:20:10 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backend.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
